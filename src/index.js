86691242967694f596eb4fa5650f95734ce655f1
Just-Jojo <62262765+Just-Jojo@users.noreply.github.com>
2021-08-31 17:32:27 -0400

    [Core] Change import logic

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@
 /**A very basic emplimentation of Kreusada's OceanScript in JavaScript
  * I mostly wrote this just to sharpen my JavaScript skills but also so that OceanScript could come to JavaScript
 */
-const encoding = require("./oceanscript/ocs.js");
+const { encode, decode } = require("./oceanscript/ocs.js");
 
 module.exports = {
-    encode: encoding.encode,
-    decode: encoding.decode,
+    encode: encode,
+    decode: decode,
     UnsupportedCharacterError: require("./oceanscript/errors"),
     ParserError: require("./oceanscript/errors")
 };
